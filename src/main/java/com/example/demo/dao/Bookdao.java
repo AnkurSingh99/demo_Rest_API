8eaafff3162d5bd9e0e9a7a469bff4d1ea80c22a
Ankur Kumar Singh <74826244+AnkurSingh99@users.noreply.github.com>
2021-08-23 02:08:59 +0530

    Additional Commit

--- src/main/java/com/example/demo/dao/Bookdao.java
+++
@@ -1,9 +0,0 @@
-/*package com.example.demo.dao;
-import com.example.demo.model.Book;
-import java.util.*;
-public interface Bookdao {
-    int insertBook(Book book);
-    List<Book> selectAllBook();
-}
-
- */
